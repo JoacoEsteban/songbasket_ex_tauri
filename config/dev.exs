bd8e280b2a6da7c863e800197c44fd39b3ee9c17
Joaquin Esteban <dev@joaco.io>
2025-07-04 18:37:48 +0200

    config

--- config/dev.exs
+++ config/dev.exs
@@ -7,7 +7,7 @@ config :songbasket_ex_tauri, SongbasketExTauri.Repo,
   stacktrace: true,
   show_sensitive_data_on_connection_error: true
 
-config :songbasket_ex_tauri, SongbasketExTauri.Api, api_url: "http://localhost:5000"
+config :songbasket_ex_tauri, SongbasketExTauri.Api, api_url: "http://127.0.0.1:5000"
 
 # For development, we disable any cache and enable
 # debugging and code reloading.
